8243cb0f12fb8b73f63caa365cf714f5dd2f35f3
GianMassari <gian.massari@davinci.edu.ar>
2020-08-26 22:25:16 -0300

    agregado

---
+++ ejercicios-guia/ejer11,12,13.js
@@ -0,0 +1,45 @@
+class Persona{
+    constructor(nombre,edad){
+        this.nombre=nombre;
+        this.edad=edad;
+    }
+
+    presentarse(){
+        return "Hola mi nombre es " + this.nombre + " Tengo " + this.edad + " años";
+    }
+}
+
+class Estudiante extends Persona{
+
+    constructor(nombre,edad){
+        super(nombre,edad)
+    }
+
+    estudiando(){
+        return "Estudiando con " + this.profesor;
+    }
+
+    setProfesor(profesor){
+        this.profesor=profesor;
+    }
+}
+
+class Profesor extends Persona{
+ constructor(){
+    var estudiantes=[];   
+ }
+enseñando(){
+console.log(estudiantes); 
+}
+addEstudiante(unEstudiante){
+estudiantes.push(unEstudiante);
+unEstudiante.setProfesor(this.nombre);
+}
+}
+
+
+
+var gian = new Estudiante("Gian marco",20);
+gian.presentarse();
+gian.setProfesor("carlos");
+gian.estudiando();
