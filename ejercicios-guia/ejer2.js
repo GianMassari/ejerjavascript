8a2c1e04161213de988981d04ed42d5d0762375b
GianMassari <gian.massari@davinci.edu.ar>
2020-09-28 18:55:25 -0300

    correccion

--- ejercicios-guia/ejer2.js
+++ ejercicios-guia/ejer2.js
@@ -1,2 +1,5 @@
 var meses=["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"];
-console.log(meses);
+meses.forEach(mes => {
+    console.log(mes);
+});
+
